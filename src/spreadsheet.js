413a0c8945f2051b94a11043cfb96e8f70976d9e
Leogabii <122978943+Leogabii@users.noreply.github.com>
2024-12-07 13:33:21 -0300

    Create spreadsheet.js

---
+++ src/spreadsheet.js
@@ -0,0 +1,21 @@
+const {GoogleSpreadsheet} = require ('google-spreadsheet');
+const credenciales = require('./json/credenciales");
+
+let googleId = "";
+
+async function accederGoogleSheet(){
+
+  const documento = new GoogleSpreadSheet(googleId);
+
+  await documento.use.ServiceAccountAuth(credenciales);
+
+  await documneto.loadInfo();
+
+  const sheet = documento.sheetByIndex[0];
+}
+accederGoogleSheet();
+
+module.exports={
+  accederGoogleSheet : accederGoogleSheet,
+}
+                             
