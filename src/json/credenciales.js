fac13e85d77238fff385804cdddaffa479b063c2
Leogabii <122978943+Leogabii@users.noreply.github.com>
2024-12-07 14:19:15 -0300

    Update credenciales.js

--- src/json/credenciales.js
+++ src/json/credenciales.js
@@ -17,7 +17,8 @@ const credenciales = {
     token_uri: process.env.TOKEN_URI,
     auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
     client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-    universe_domain: process.env.UNIVERSE_DOMAIN
+    universe_domain: process.env.UNIVERSE_DOMAIN,
+    googleId: process.env.GOOGLE_ID
 };
 
 export default credenciales;
